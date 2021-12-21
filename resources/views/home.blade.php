ed0e71e1398a635a2a92e67140ec062824162315
rocatoro18 <rocatoro18@outlook.com>
2021-12-20 20:44:11 -0700

    Se agregaron fuentes y se agrego un menu de inicio con iconos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Menú') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,26 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <div class="row text-center">
+                        <div class="col-sm-4">
+                            <a href="/administrar">
+                            <h4>Administrar</h4>
+                            <img width="50px" src="{{asset('images/administrar.png')}}"/>
+                            </a>
+                        </div>
+                        <div class="col-sm-4">
+                            <a href="/cajero">
+                            <h4>Cajero</h4>
+                            <img width="50px" src="{{asset('images/cajero.png')}}"/>
+                            </a>
+                        </div>
+                        <div class="col-sm-4">
+                            <a href="/reporte">
+                            <h4>Reporte</h4>
+                            <img width="50px" src="{{asset('images/reporte.png')}}"/>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
