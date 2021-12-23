dbe5047149481196a4cccd6177b08f69d400026b
rocatoro18 <rocatoro18@outlook.com>
2021-12-22 21:42:23 -0700

    Inicio modulo cajero

---
+++ app/Http/Controllers/Cajero/CajeroController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Cajero;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CajeroController extends Controller
+{
+    // Primera página de cajero
+    public function index(){
+        return view('cajero.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/cajero', function () {
+    return view('cajero.index');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
